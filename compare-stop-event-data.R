4bce95919ea50e14f120a0024722434ec0227b9c
Gracielle Higino <graciellehigino@hotmail.com>
2023-11-08 19:12:45 -0800

    trick subtitle compare-stop-event-data.R
    My tip to fix your subtitle!

--- compare-stop-event-data.R
+++ compare-stop-event-data.R
@@ -4,13 +4,18 @@ library(ggthemes)
 
 stop_event_data <- read_rds("data/stop_events.rds") 
 
+# I first created an object to store the route description. You can do that in many different ways, I chose to use "distinct" to get all the unique descriptions. If we had more than one, we could access these names using its index.
+
+route_name <- stop_event_data |> 
+  distinct(public_route_description)
+
 stop_event_data |> 
   ggplot(mapping=aes(x=total_trip_time_seconds,fill=public_direction_description)) +
   geom_histogram(bins=10, show.legend = FALSE) +
   facet_grid(cols = vars(public_direction_description), rows = vars(service_date)) +
   theme_minimal(base_size=14) +
   labs(title = "Trip Duration In Seconds By Directions By Date",
-       subtitle = "Route [Insert Route Description Here]",
+       subtitle = str_glue("Route {route_name}"), # here's your trick!
        x=NULL, y=NULL, fill=NULL)
 
 
